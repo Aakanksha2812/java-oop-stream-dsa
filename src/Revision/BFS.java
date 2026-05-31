630de5771d9c856f4b74abbde51fff2479cf301f
Aakanksha2812 <shindeaakanksha28@gmail.com>
2026-05-31 11:06:33 +0530

    BFS Revision

--- src/Revision/BFS.java
+++ src/Revision/BFS.java
@@ -21,23 +21,52 @@ public class BFS {
                 }
             }
         }
-        int[] dx=new int[]{1,-1,0,0};
-        int[] dy=new int[]{0,0,1,-1};
-        while (!q.isEmpty()){
-            int[] pol=q.poll();
-            int row=pol[0];
-            int col=pol[1];
-            for (int d = 0; d <4 ; d++) {
-                int ni=row+dx[d];
-                int nj=col+dy[d];
-                if (ni>=0 && nj>=0 && ni<n && nj<m && !visited[ni][nj]){
-                    grid[ni][nj]=grid[row][col]+1;
-                    q.add(new int[]{ni,nj});
-                    visited[ni][nj]=true;
+        int[] dx = new int[]{1, -1, 0, 0};
+        int[] dy = new int[]{0, 0, 1, -1};
+        while (!q.isEmpty()) {
+            int[] pol = q.poll();
+            int row = pol[0];
+            int col = pol[1];
+            for (int d = 0; d < 4; d++) {
+                int ni = row + dx[d];
+                int nj = col + dy[d];
+                if (ni >= 0 && nj >= 0 && ni < n && nj < m && !visited[ni][nj]) {
+                    grid[ni][nj] = grid[row][col] + 1;
+                    q.add(new int[]{ni, nj});
+                    visited[ni][nj] = true;
                 }
             }
         }
         return grid;
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n = image.length;
+        int m = image[0].length;
+        int c = image[sr][sc];
+        if (c == color) {
+            return image;
+        }
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{sr, sc});
+        image[sr][sc]=color;
+        int[] dx = new int[]{1, -1, 0, 0};
+        int[] dy = new int[]{0, 0, 1, -1};
+        while (!q.isEmpty()) {
+            int[] pol = q.poll();
+            int row = pol[0];
+            int col = pol[1];
+            for (int d = 0; d < 4; d++) {
+                int ni = row + dx[d];
+                int nj = col + dy[d];
+                if (ni >=0 && nj >= 0 && ni < n && nj < m && image[ni][nj] == c) {
+                    image[ni][nj] = color;
+                    q.add(new int[]{ni, nj});
+
+                }
+            }
+        }
+        return image;
     }
 
     public static void main(String[] args) {
@@ -49,5 +78,16 @@ public class BFS {
             printArrayMethod(arr);
             System.out.println();
         }
+        int[][] image = new int[][]
+                {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+        int[][] ans1 = b.floodFill(image, 1, 1, 2);
+        System.out.println(
+                "fill color"
+        );
+        for (int[] arr : ans1) {
+            printArrayMethod(arr);
+            System.out.println();
+        }
+
     }
 }
