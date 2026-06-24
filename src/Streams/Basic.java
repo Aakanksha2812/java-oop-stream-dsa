c554d194ff6305478dd529adcd7724c7a290a6a0
Aakanksha2812 <shindeaakanksha28@gmail.com>
2026-06-24 16:41:44 +0530

    Streams practice

--- src/Streams/Basic.java
+++ src/Streams/Basic.java
@@ -1,7 +1,9 @@
 package Streams;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Basic {
@@ -32,6 +34,30 @@ public class Basic {
         return nums.stream().filter(n -> n > 2).map(n -> n * n).collect(Collectors.toCollection(ArrayList::new));
     }
 
+    Map<Integer, List<String>> sameWordLength() {
+        List<String> words = List.of("hi", "hello", "hey", "world", "ok");
+        return words.stream().collect(Collectors.groupingBy(String::length));
+    }
+
+    Map<Character, Long> sameCharacterCount() {
+        List<String> names = List.of("Alice", "Bob", "Anna", "Charlie", "Ben", "Amy");
+
+        return names.stream().collect(Collectors.groupingBy(s -> s.charAt(0), Collectors.counting()));
+    }
+
+    List<Integer> singleList() {
+        List<List<Integer>> nested = List.of(
+                List.of(1, 2, 3),
+                List.of(4, 5),
+                List.of(6, 7, 8, 9)
+        );
+        return nested.stream().flatMap(Collection::stream).collect(Collectors.toList());
+    }
+
+    String joinByComma() {
+        List<String> words = List.of("apple", "banana", "cherry", "date");
+        return words.stream().collect(Collectors.joining(","));
+    }
 
     public static void main(String[] args) {
         Basic b = new Basic();
@@ -40,6 +66,10 @@ public class Basic {
         System.out.println("String of list converted to uppercase " + b.convertToUppercase());
         System.out.println("sum of digit greater than 3 " + b.sumGreaterDigitThree());
         System.out.println("List of names who start with A " + b.nameWithA());
-        System.out.println("List of square of digit greater than two "+b.squareOfGreaterDigitTwo());
+        System.out.println("List of square of digit greater than two " + b.squareOfGreaterDigitTwo());
+        System.out.println("group by same length: " + b.sameWordLength());
+        System.out.println("Count Same intial character: " + b.sameCharacterCount());
+        System.out.println("convert nested list to flat list: " + b.singleList());
+        System.out.println("join by comma: "+b.joinByComma());
     }
 }
