7ca3184e85cc93db106d53e932b89063bc26b79d
Aakanksha2812 <shindeaakanksha28@gmail.com>
2026-07-08 10:53:54 +0530

    LinedList practice

--- src/LinkedList/ListNodeOperations.java
+++ src/LinkedList/ListNodeOperations.java
@@ -116,18 +116,59 @@ public class ListNodeOperations {
     }
 
     public ListNode deleteDuplicates(ListNode head) {
-     ListNode temp=head;
-     while (temp!=null && temp.next!=null){
-         while (temp!=null && temp.next!=null && temp.val==temp.next.val){
-             temp.next=temp.next.next;
-         }
-         temp=temp.next;
-     }
-     return head;
+        ListNode temp = head;
+        while (temp != null && temp.next != null) {
+            while (temp != null && temp.next != null && temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        int lenA = 0;
+        int lenB = 0;
+        int pos = 0;
+        while (p1 != null) {
+            lenA++;
+            p1 = p1.next;
+        }
+        while (p2 != null) {
+            lenB++;
+            p2 = p2.next;
+        }
+        p1 = headA;
+        p2 = headB;
+        if (lenA > lenB) {
+            pos = lenA - lenB;
+            while (pos > 0) {
+                pos--;
+                p1 = p1.next;
+            }
+        } else {
+            pos = lenB - lenA;
+            while (pos > 0) {
+                pos--;
+                p2 = p2.next;
+            }
+        }
+        while (p1 != null && p2 != null) {
+            if (p1 == p2) {
+                return p1;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return null;
+
+
     }
 
     public static void main(String[] args) {
-       ListNode l1 = new ListNode(1);
+        ListNode l1 = new ListNode(1);
         ListNode l2 = new ListNode(4);
         ListNode l3 = new ListNode(6);
         ListNode l4 = new ListNode(2);
@@ -145,16 +186,16 @@ public class ListNodeOperations {
         // l1 = op.middleNodeSlowFastPointer(l1);
         //     System.out.println("Linked List Contain cycle: " + op.hasCycle(l1));
         //   System.out.println("cycle detected at : " + op.detectCycle(l1).val);
-      //  l1 = op.mergeTwoLists(l1, l4);
+        //  l1 = op.mergeTwoLists(l1, l4);
 
         ListNode l7 = new ListNode(1);
-        ListNode l8= new ListNode(1);
+        ListNode l8 = new ListNode(1);
         ListNode l9 = new ListNode(1);
-      //  ListNode l10 = new ListNode(9);
-        l7.next=l8;
-        l8.next=l9;
-      //  l9.next=l10;
-        l7=op.deleteDuplicates(l7);
+        //  ListNode l10 = new ListNode(9);
+        l7.next = l8;
+        l8.next = l9;
+        //  l9.next=l10;
+        l7 = op.deleteDuplicates(l7);
         l7.printLL();
     }
 }
