f1474d22ac540c6a3625d57d5ef0380125a689b4
Aakanksha2812 <shindeaakanksha28@gmail.com>
2026-07-08 10:12:28 +0530

    LinedList practice

--- src/LinkedList/ListNodeOperations.java
+++ src/LinkedList/ListNodeOperations.java
@@ -70,10 +70,10 @@ public class ListNodeOperations {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) {
-                ListNode ptr=head;
+                ListNode ptr = head;
                 while (ptr != slow) {
-                    ptr=ptr.next;
-                    slow=slow.next;
+                    ptr = ptr.next;
+                    slow = slow.next;
                 }
                 return slow;
             }
@@ -81,25 +81,80 @@ public class ListNodeOperations {
         return null;
     }
 
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+
+                tail.next = list1;
+
+                list1 = list1.next;
+
+            } else {
+
+                tail.next = list2;
+
+                list2 = list2.next;
+            }
+
+            tail = tail.next;
+        }
+
+        if (list1 != null) {
+            tail.next = list1;
+
+        }
+        if (list2 != null) {
+            tail.next = list2;
+
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+     ListNode temp=head;
+     while (temp!=null && temp.next!=null){
+         while (temp!=null && temp.next!=null && temp.val==temp.next.val){
+             temp.next=temp.next.next;
+         }
+         temp=temp.next;
+     }
+     return head;
+    }
+
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(1);
-        ListNode l2 = new ListNode(2);
-        ListNode l3 = new ListNode(3);
-        ListNode l4 = new ListNode(4);
+       ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(4);
+        ListNode l3 = new ListNode(6);
+        ListNode l4 = new ListNode(2);
         ListNode l5 = new ListNode(5);
-        //  ListNode l6 = new ListNode(6);
+        ListNode l6 = new ListNode(9);
         l1.next = l2;
         l2.next = l3;
-        l3.next = l4;
+
         l4.next = l5;
-        l5.next = l2;
-        //  l5.next=l6;
+        //  l5.next = l2;
+        l5.next = l6;
         ListNodeOperations op = new ListNodeOperations();
         // l1 = op.reverse(l1);
         //   l1 = op.middleNode(l1);
         // l1 = op.middleNodeSlowFastPointer(l1);
-        System.out.println("Linked List Contain cycle: " + op.hasCycle(l1));
-        System.out.println("cycle detected at : " + op.detectCycle(l1).val);
-        //  l1.printLL();
+        //     System.out.println("Linked List Contain cycle: " + op.hasCycle(l1));
+        //   System.out.println("cycle detected at : " + op.detectCycle(l1).val);
+      //  l1 = op.mergeTwoLists(l1, l4);
+
+        ListNode l7 = new ListNode(1);
+        ListNode l8= new ListNode(1);
+        ListNode l9 = new ListNode(1);
+      //  ListNode l10 = new ListNode(9);
+        l7.next=l8;
+        l8.next=l9;
+      //  l9.next=l10;
+        l7=op.deleteDuplicates(l7);
+        l7.printLL();
     }
 }
