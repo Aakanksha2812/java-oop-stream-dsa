28c37542b356eb9928de32fd4741114d5333fe7c
Aakanksha2812 <shindeaakanksha28@gmail.com>
2026-07-12 13:03:34 +0530

    Tree practice

---
+++ src/Tree/TreeOperations.java
@@ -0,0 +1,34 @@
+package Tree;
+
+public class TreeOperations {
+    void preOrder(TreeNode root){
+        if(root==null){
+            return;
+        }
+        System.out.println(root.val+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode leftNode = new TreeNode(2);
+        TreeNode rightNode = new TreeNode(3);
+        TreeNode node1 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(5);
+        TreeNode node3 = new TreeNode(6);
+        TreeNode node4 = new TreeNode(7);
+        TreeNode node5 = new TreeNode(8);
+        TreeNode node6 = new TreeNode(9);
+        root.left = leftNode;
+        root.right = rightNode;
+        leftNode.left = node1;
+        leftNode.right = node2;
+        rightNode.left = node3;
+        rightNode.right = node4;
+        node1.left=node5;
+        node1.right=node6;
+        TreeOperations tree=new TreeOperations();
+        tree.preOrder(root);
+
+    }
+}
